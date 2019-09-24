bcd202b59326104968b3f713bbb7137fa615b160
Aragorn <franklin@anincubator.com>
2019-09-24 16:16:45 -0400

    commit

--- src/global.c
+++ src/global.c
@@ -26,7 +26,7 @@ Global NewGlobal()
 
     // configuraciones para la grilla.
     global.spaceGrid2D = 16; 
-    global.numLinesGrid2D = 16;
+    global.numLinesGrid2D = 32;
 
     global.keySectionCanvas = KEY_F1;
     global.keySectionMap = KEY_F2;
